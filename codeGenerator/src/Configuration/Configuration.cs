b5bf6b4736268f30bb7c26fbe3bf5cdd1819a851
Nick Frisch <nfrisch3@outlook.com>
2023-06-19 19:20:31 -0400

    Moving templates from xml to individual files

--- codeGenerator/src/Configuration/Configuration.cs
+++ codeGenerator/src/Configuration/Configuration.cs
@@ -11,46 +11,19 @@ namespace Configuration
     [Serializable]
     public class toolConfiguration
     {
+        [XmlIgnore]
+        public string configurationFullPath = "";
+
         public string rootOutputFolder = "";
         public string robotConfiguration = "";
         public List<string> robotConfigurations = new List<string>();
         public List<string> treeviewParentNameExtensions = new List<string>();
 
-        public string AllMechanisms_h = "";
-        public string AllMechanismsState_h = "";
-        public string AllMechanismsStateMgr_h = "";
-
-        public string stateManager_h = "";
-        public string stateManager_cpp = "";
-
-        public string state_h = "";
-        public string state_cpp = "";
-
-        public string main_h = "";
-        public string main_cpp = "";
-
-        public string SolenoidUsage_h = "";
-        public string SolenoidUsage_cpp = "";
-
-        public string MotorControllerUsage_h = "";
-        public string MotorControllerUsage_cpp = "";
-
-        public string DigitalInputUsage_h = "";
-        public string DigitalInputUsage_cpp = "";
-
-        public string ServoUsage_h = "";
-        public string ServoUsage_cpp = "";
-
-        public string MechanismTypes_h = "";
-        public string MechanismTypes_cpp = "";
-
-        public string StateStruc_h = "";
-
-        public string RobotConfig_h = "";
+        public string templateMechanismCppPath = "";
+        public string templateMechanismHPath = "";
 
         public string CopyrightNotice = "";
-
-        public string GenerationNotice = ""; 
+        public string GenerationNotice = "";
 
         public void loadDummyData()
         {
@@ -78,6 +51,8 @@ namespace Configuration
         public toolConfiguration deserialize(string fullFilePathName)
         {
             var mySerializer = new XmlSerializer(typeof(toolConfiguration));
+
+            configurationFullPath = fullFilePathName;
 
             using (var myFileStream = new FileStream(fullFilePathName, FileMode.Open))
             {
