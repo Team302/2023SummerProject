1f1ff9d53ce23754b6d5e628bf89961390671dbc
David Muscat <muscatd75@gmail.com>
2023-09-08 20:53:01 -0400

    created some basic tests

--- codeGenerator/src/UnitTests/UnitTests.cs
+++ codeGenerator/src/UnitTests/UnitTests.cs
@@ -3,6 +3,8 @@ using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace UnitTests
@@ -13,13 +15,26 @@ namespace UnitTests
         private const string WinAppDriver = @"C:\Program Files\Windows Application Driver\WinAppDriver.exe";
         private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
         private const string applicationFullPathName = @"C:\FRC\2023SummerProject\codeGenerator\src\FRCrobotCodeGen302\bin\Debug\FRCrobotCodeGen302.exe";
-
         private static WindowsDriver<WindowsElement> Session { get; set; }
         private static Process theWinAppDriverProcess = null;
+
+        //============================ configurable settings
+        private const string testConfigurationFile = "testConfiguration.xml";
+        private static string baseTestDirectory = "guiTests";
+        private static string referenceFilesDirectory = "referenceFiles";
+        private static string configDirectory = "configuration";
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext testContext)
         {
+            string codeGeneratorDir = Path.GetFullPath(Path.Combine(testContext.TestDir, "..","..",".."));
+            baseTestDirectory = Path.Combine(codeGeneratorDir, baseTestDirectory);
+            referenceFilesDirectory = Path.Combine(baseTestDirectory, referenceFilesDirectory);
+            configDirectory = Path.Combine(baseTestDirectory, configDirectory);
+            Console.WriteLine(baseTestDirectory);
+            Console.WriteLine(referenceFilesDirectory);
+            Console.WriteLine(configDirectory);
+
             theWinAppDriverProcess = Process.Start(WinAppDriver);
             Thread.Sleep(TimeSpan.FromSeconds(2)); // to allow time for the win app driver to start 
 
@@ -49,22 +64,38 @@ namespace UnitTests
                 theWinAppDriverProcess.Kill();
         }
 
+        /// <summary>
+        /// Creates a new Robot variants configuration in a clean directory
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_00000_CreateNewConfiguration()
         {
-            Session.FindElementByName("Configuration").Click();
+            string currentDir = Directory.GetCurrentDirectory();
+            
+            string filePathName = Path.Combine(configDirectory, testConfigurationFile);
+
+            if (Directory.Exists(configDirectory))
+                Directory.Delete(configDirectory, true);
+            Directory.CreateDirectory(configDirectory);
+
+            Session.FindElementByName("Main").Click();
 
             Thread.Sleep(TimeSpan.FromSeconds(0.5)); // time to switch tabs
 
-            Session.FindElementByAccessibilityId("nodePathSelectorTextBox").Click();
-            Session.FindElementByAccessibilityId("nodePathSelectorTextBox").SendKeys("Robot Variant/robots/Robot #1");
-            Session.FindElementByAccessibilityId("selectNodeButton").Click();
+            Session.FindElementByAccessibilityId("createNewRobotVariantsConfigButton").Click();
+            Thread.Sleep(TimeSpan.FromSeconds(1)); // wait a bit for the save file dialog to pop up
+            Session.FindElementByXPath("//Edit[@Name='File name:']").SendKeys(filePathName);
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            Session.FindElementByName("Save").Click();
 
-            Thread.Sleep(TimeSpan.FromSeconds(5)); // to give us time to see what is going on 
+            Thread.Sleep(TimeSpan.FromSeconds(1)); // wait for the file to be written
+            Assert.IsTrue(File.Exists(filePathName));
+
+            Thread.Sleep(TimeSpan.FromSeconds(15)); // to give us time to see what is going on 
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void TestMethod_00001_()
         {
             Session.FindElementByName("Configuration").Click();
 
@@ -75,7 +106,7 @@ namespace UnitTests
             Session.FindElementByAccessibilityId("nodePathSelectorTextBox").SendKeys("Robot Variant/mechanisms/Turret");
             Session.FindElementByAccessibilityId("selectNodeButton").Click();
 
-            Thread.Sleep(TimeSpan.FromSeconds(15)); // to give us time to see what is going on 
+            Thread.Sleep(TimeSpan.FromSeconds(5)); // to give us time to see what is going on 
         }
     }
 }
